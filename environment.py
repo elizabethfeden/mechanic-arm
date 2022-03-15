55ca3c5c86eb864ceee4f8ce3de235d378cc9302
Elizabeth <52410353+elizabethfeden@users.noreply.github.com>
2022-03-15 10:42:47 +0300

    Create environment.py

---
+++ environment.py
@@ -0,0 +1,18 @@
+import gym
+
+class Environment(gym.Env):
+  def __init__(self):
+    super(Environment, self).__init__()
+    
+    self.observation_space = #
+    self.action_space = #
+    self.canvas = #
+    
+  def reset(self):
+    pass
+    
+  def render(self):
+    pass
+    
+  def step(self, action):
+    pass
